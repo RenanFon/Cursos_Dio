9d8cb0753a8e8e925521e7563491cd2986cd4be8
Renan_Fonseca <renanfonseca88@gmail.com>
2022-02-07 19:33:42 -0300

    fdew

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -14,3 +14,48 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+const bote = document.getElementById('bot')
+
+function mostrar(){
+    bote.classList.toggle('active')
+}
+
+
+bote.addEventListener('click',mostrar)
+
+
+
+function mudartela(event){
+    if(event.key === 'v'){
+        document.body.classList.toggle('telacheia')
+
+    }
+
+
+}
+
+window.addEventListener('keydown',mudartela)
+
+
+const titulo = document.querySelector('h3')
+
+
+const imgs = document.querySelectorAll('img')
+
+function passar(event){
+    console.log(event.target)
+}
+
+
+
+imgs.forEach((img)=>{
+    img.addEventListener('click',passar)
+})
+
+
+const titulo1 = document.querySelector('h3')
+
+
+
